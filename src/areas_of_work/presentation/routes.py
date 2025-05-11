edeae1b33f3baa541f5f0dc2143cec9a231fc378
Nishchit Bhandari <nishchitbhandari2061@gmail.com>
2025-05-11 12:27:32 +0545

    Change the output schema where the results are inside data key

--- src/areas_of_work/presentation/routes.py
+++ src/areas_of_work/presentation/routes.py
@@ -1,23 +1,26 @@
+from src.areas_of_work.domain.entities import AreasOfWork, AreasOfWorkUpdate, AreasListResponse, AreasOfWorkResponse
 from src.areas_of_work.application.areas_of_work_use_cases import AreaUseCases
 from src.areas_of_work.presentation.config import get_areas_of_work_use_cases
-from src.areas_of_work.domain.entities import AreasOfWork, AreasOfWorkUpdate
 from src.areas_of_work.presentation.config import areas_router
 from src.auth.domain.entities.users_entity import UserOut
 from src.auth.presentation.config import get_current_user
 from fastapi import status, Depends
 
 
-@areas_router.get("/", response_model=list[AreasOfWork], status_code=status.HTTP_200_OK)
+@areas_router.get("/", response_model=AreasListResponse, status_code=status.HTTP_200_OK)
 def get_areas_of_work(
         use_cases: AreaUseCases = Depends(get_areas_of_work_use_cases)
 ):
     """ 
     Gets all areas of work.
     """
-    return use_cases.get_all_areas()
+    result = use_cases.get_all_areas()
+    return {
+        "data": result
+    }
 
 
-@areas_router.get("/{area_id}", response_model=AreasOfWork, status_code=status.HTTP_200_OK)
+@areas_router.get("/{area_id}", response_model=AreasOfWorkResponse, status_code=status.HTTP_200_OK)
 def get_one_area(
         area_id: str,
         use_cases: AreaUseCases = Depends(get_areas_of_work_use_cases)
@@ -25,10 +28,13 @@ def get_one_area(
     """ 
     Gets one area of work.
     """
-    return use_cases.get_one_area(area_id)
+    result = use_cases.get_one_area(area_id)
+    return {
+        "data": result
+    }
 
 
-@areas_router.post("/", response_model=AreasOfWork, status_code=status.HTTP_201_CREATED)
+@areas_router.post("/", response_model=AreasOfWorkResponse, status_code=status.HTTP_201_CREATED)
 def create_area(
         content: AreasOfWork,
         current_user: UserOut = Depends(get_current_user),
@@ -37,10 +43,13 @@ def create_area(
     """ 
     Creates an area of work.
     """
-    return use_cases.create_area(content, current_user)
+    result = use_cases.create_area(content, current_user)
+    return {
+        "data": result
+    }
 
 
-@areas_router.patch("/{area_id}", status_code=status.HTTP_201_CREATED, response_model=AreasOfWork)
+@areas_router.patch("/{area_id}", status_code=status.HTTP_201_CREATED, response_model=AreasOfWorkResponse)
 def update_area(
         area_id: str,
         content: AreasOfWorkUpdate,
@@ -51,7 +60,9 @@ def update_area(
     Updates an area of work.
     """
     message = use_cases.update_area(area_id, content, current_user)
-    return message
+    return {
+        "data": message
+    }
 
 
 @areas_router.delete("/{area_id}", status_code=status.HTTP_200_OK)
